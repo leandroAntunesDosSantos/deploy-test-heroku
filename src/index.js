1ea6f67cf2a00bd84135597ef05686d9d8dc201c
leandro <leandro_antunes28@hotmail.com>
2024-04-27 14:53:55 -0300

    configurando requisicao categorias

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 require('dotenv').config()
 const express = require('express')
 
+const knex = require('./conexao')
+
 const app = express()
 
 app.use(express.json())
 
 
 
-app.get('/', (req, res) => {
-    return res.send('Hello World!')
+app.get('/', async (req, res) => {
+    try{
+        const categoria = await knex('categorias')
+
+        return res.json(categoria)
+    }catch(error){
+        console.log(error)
+        return res.status(400).json({erro: error.message})
+    }
 })
 
 app.listen(process.env.PORT , () => {
